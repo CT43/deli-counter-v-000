1ef4b9fd52ee56b55db203664a4004da252c97cc
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-22 19:23:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,8 @@ def line (order)
    else
      line_start = "The line is currently"
      counter = 0
-     until counter == order.size do |customer|
-       line_start << " #{counter}. #{customer}"
+     until counter == order.size do
+       line_start << " #{counter}. order[#{counter}]"
        counter += 1
      end
      puts line_start
