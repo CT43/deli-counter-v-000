764322a3e3005963967e0376069844ef1bc753e9
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-22 19:36:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line (order)
     end
 end
 
-def take_a_number (array, name)
-  array << name
+def take_a_number (katz_deli, "name")
+  katz_deli << name
   puts "Welcome. #{name}. You are number #{array.length} in line."
 end
