d56ee07aff991c02add427ed96b8f1af99237136
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-22 19:02:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 katz_deli = []
 
 def line (katz_deli)
-  if katz_deli.length == 0 
-     return "The line is currently empty." 
+  if katz_deli.length == 0
+     puts "The line is currently empty."
    else
       return katz_deli
     end
