70dc2a5b7028c6b6941373aa51c48e9253aec7cb
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-22 19:15:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,17 @@
+require 'pry'
 katz_deli = []
 
 def line (katz_deli)
   if katz_deli.length == 0
      puts "The line is currently empty."
    else
-      puts katz_deli.join(", ")
+      all_line = []
+      counter = 0
+       until all_line.size = katz_deli.size
+         counter += 1
+         a = katz_deli.shift
+         all_line << a
+       end
+       binding.pry 
     end
 end
